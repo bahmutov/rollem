96c16fdc6907e2a136f9c62ed92dabc43a5d72a1
YuryStrozhevsky <yury@strozhevsky.com>
2016-12-14 14:34:11 +0300

    Syntax clean-up

--- src/index.js
+++ src/index.js
@@ -1,63 +1,74 @@
-'use strict'
+'use strict';
 
-const debug = require('debug')('rollem')
-const rollup = require('rollup')
-const R = require('ramda')
-const mergeFolders = require('./merge-folders').merge
+const debug = require('debug')('rollem');
+const rollup = require('rollup');
+const R = require('ramda');
+const mergeFolders = require('./merge-folders').merge;
 
-function buildBundle (config) {
-  return rollup.rollup(config)
-    .then(function (bundle) {
-      return bundle.write(config).then(() => config.dest)
-    })
+function buildBundle(config)
+{
+	return rollup.rollup(config)
+		.then(function(bundle)
+		{
+			return bundle.write(config).then(() => config.dest)
+		});
 }
 
-function buildBundles (configs) {
-  const promises = configs.map(buildBundle)
-  return Promise.all(promises)
-    .then((bundles) => {
-      console.log('[%s] built %d bundles', new Date().toTimeString(), configs.length)
-      debug(bundles)
-      return bundles
-    })
-    .catch((err) => {
-      console.error(err.stack)
-    })
+function buildBundles(configs)
+{
+	const promises = configs.map(buildBundle);
+	return Promise.all(promises)
+		.then(bundles =>
+		{
+			console.log('[%s] built %d bundles', new Date().toTimeString(), configs.length);
+			debug(bundles);
+			return bundles;
+		})
+		.catch(err =>
+		{
+			console.error(err.stack);
+		})
 }
 
-function collectInputFolders (configs) {
-  const filenames = R.map(R.prop('entry'), configs)
-  return mergeFolders(filenames)
+function collectInputFolders(configs)
+{
+	const filenames = R.map(R.prop('entry'), configs);
+	return mergeFolders(filenames)
 }
 
-function rollem (configs, options) {
-  options = options || {}
-  debug('Rollem configs')
-  debug(configs)
-  debug('Rollem options', options)
-
-  console.assert(Array.isArray(configs), 'expected list of configs')
-
-  if (options.watch) {
-    const folders = collectInputFolders(configs)
-    console.log('[%s] watching source folders for changes', new Date().toTimeString(), folders)
-
-    const watch = require('watch')
-    const EventEmitter = require('events')
-    const watcher = new EventEmitter()
-
-    folders.forEach((sourceFolder) => {
-      watch.watchTree(sourceFolder, function onFileChange () {
-        // will be called on the initial setup
-        watcher.emit('changed')
-        buildBundles(configs)
-          .then(() => watcher.emit('rolled'))
-      })
-    })
-    return Promise.resolve(watcher)
-  } else {
-    return buildBundles(configs)
-  }
+function rollem(configs, options)
+{
+	options = options || {};
+	debug('Rollem configs');
+	debug(configs);
+	debug('Rollem options', options);
+	
+	console.assert(Array.isArray(configs), 'expected list of configs');
+	
+	if(options.watch)
+	{
+		const folders = collectInputFolders(configs);
+		console.log('[%s] watching source folders for changes', new Date().toTimeString(), folders);
+		
+		const watch = require('watch');
+		const EventEmitter = require('events');
+		const watcher = new EventEmitter();
+		
+		folders.forEach(sourceFolder =>
+		{
+			watch.watchTree(sourceFolder, function onFileChange()
+			{
+				// will be called on the initial setup
+				watcher.emit('changed');
+				buildBundles(configs)
+					.then(() => watcher.emit('rolled'))
+			})
+		});
+		return Promise.resolve(watcher)
+	}else
+	{
+		return buildBundles(configs)
+	}
 }
 
-module.exports = rollem
+module.exports = rollem;
--- src/merge-folders.js
+++ src/merge-folders.js
@@ -1,32 +1,37 @@
-'use strict'
+'use strict';
 
-const R = require('ramda')
-const path = require('path')
+const R = require('ramda');
+const path = require('path');
 
-function isParentFolder (parentPath, childPath) {
-  const relative = path.relative(parentPath, childPath)
-  return relative && !relative.startsWith('..')
+function isParentFolder(parentPath, childPath)
+{
+	const relative = path.relative(parentPath, childPath);
+	return relative && !relative.startsWith('..');
 }
 
-function isChildFolder (childPath, parentPath) {
-  return isParentFolder(parentPath, childPath)
+function isChildFolder(childPath, parentPath)
+{
+	return isParentFolder(parentPath, childPath)
 }
 
-function mergeWatchedFolders (filenames) {
-  const folders = R.map(path.dirname, filenames)
-  const cleanedFolders = R.map(path.normalize, folders)
-  const uniqFolders = R.uniq(cleanedFolders)
-
-  // eliminate any folder that is a child of another folder
-  return uniqFolders.filter((folder) => {
-    return !uniqFolders.some((possibleParent) => {
-      return isChildFolder(folder, possibleParent)
-    })
-  })
+function mergeWatchedFolders(filenames)
+{
+	const folders = R.map(path.dirname, filenames);
+	const cleanedFolders = R.map(path.normalize, folders);
+	const uniqFolders = R.uniq(cleanedFolders);
+	
+	// eliminate any folder that is a child of another folder
+	return uniqFolders.filter(folder =>
+	{
+		return !uniqFolders.some(possibleParent =>
+		{
+			return isChildFolder(folder, possibleParent)
+		});
+	});
 }
 
 module.exports = {
-  isParentFolder: isParentFolder,
-  isChildFolder: isChildFolder,
-  merge: mergeWatchedFolders
-}
+	isParentFolder,
+	isChildFolder,
+	merge: mergeWatchedFolders
+};
