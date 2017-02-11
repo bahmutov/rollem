3c1228f6a2c246df1beb5b97a2476a7ebff4a793
Lajos.Meszaros <m_lajos@hotmail.com>
2017-02-11 17:19:30 +0100

    Re-enabled case in test build

--- test/with-multi-entry-plugin/rollem.config.js
+++ test/with-multi-entry-plugin/rollem.config.js
@@ -1,15 +1,12 @@
 import multiEntry from 'rollup-plugin-multi-entry'
 
-export default [
-// {
-  // entry: ['src/**/b.js', 'src/**/a.js'],
-  // dest: 'dist/case1.js',
-  // format: 'iife',
-  // moduleName: 'case1',
-  // plugins: [multiEntry()]
-// },
-
-{
+export default [{
+  entry: ['src/**/b.js', 'src/**/a.js'],
+  dest: 'dist/case1.js',
+  format: 'iife',
+  moduleName: 'case1',
+  plugins: [multiEntry()]
+}, {
   entry: ['src/case2/foo.js'],
   dest: 'dist/case2.js',
   format: 'iife',
