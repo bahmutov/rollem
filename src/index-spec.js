848f0dc22399415df478eaeb228bea4717f13f7e
Lajos.Meszaros <m_lajos@hotmail.com>
2017-02-11 14:57:36 +0100

    Tweaked merge-folders functions according to the new unit test cases

--- src/index-spec.js
+++ src/index-spec.js
@@ -64,11 +64,11 @@ describe('merge folders', () => {
     it('collapses sequential and unnecessary **/ paths while keeping the directory separator intact', () => {
       const folder1 = 'foo/**/**/**/bar'
       const cleanedFolder1 = normalizeGlob(folder1)
-      la(equals(cleanedFolder1, 'foo/**/bar'), cleanedFolder1)
+      la(equals(cleanedFolder1, 'foo/**/bar'), 'test1: ' + cleanedFolder1)
       
       const folder2 = 'foo\\**\\**\\**/bar'
       const cleanedFolder2 = normalizeGlob(folder2)
-      la(equals(cleanedFolder2, 'foo\\**\\bar'), cleanedFolder2)
+      la(equals(cleanedFolder2, 'foo\\**\\bar'), 'test2: ' + cleanedFolder2)
     })
   })
   
@@ -107,15 +107,15 @@ describe('merge folders', () => {
     it('takes ** as the parent among other folders, if it\'s on the end and compared folders have the same depth', () => {
       const p1 = 'foo/**'
       const c1 = 'foo/bar'
-      la(isChild(c1, p1), path.relative(p1, c1))
+      la(isChild(c1, p1), 'test1: ' + path.relative(p1, c1))
       
       const p2 = 'foo/**/x'
       const c2 = 'foo/bar'
-      la(!isChild(c2, p2), path.relative(p2, c2))
+      la(!isChild(c2, p2), 'test2: ' + path.relative(p2, c2))
       
       const p3 = 'foo/x/**'
       const c3 = 'foo/bar'
-      la(!isChild(c3, p3), path.relative(p3, c3))
+      la(!isChild(c3, p3), 'test3: ' + path.relative(p3, c3))
     })
   })
 })
--- src/merge-folders.js
+++ src/merge-folders.js
@@ -5,20 +5,29 @@ const path = require('path')
 
 function isParentFolder (parentPath, childPath) {
   const relative = path.relative(parentPath, childPath)
+  let retValue = false;
   
+  if (relative) {
+    if (relative.startsWith('..')) {
+      // from 'foo/**' and 'foo/bar' the first should be considered as a parent folder
+      if (relative.split(path.sep).length === 2 && parentPath.endsWith('**')) {
+        retValue = true
+      }
+    } else {
+      retValue = true
+    }
+  }
   
-  
-  /*
-  */
-  return relative && !relative.startsWith('..')
+  return retValue
 }
 
 function isChildFolder (childPath, parentPath) {
   return isParentFolder(parentPath, childPath)
 }
 
-function normalizeGlob (path) {
-  return path.replace(/\*\*([\\/])(?:\*\*[\\/])+/, '**$1')
+function normalizeGlob (folder) {
+  // collapses multiple sequential '**/' parts into a single '**/'
+  return folder.replace(/\*\*([\\/])(?:\*\*[\\/])+/, '**$1')
 }
 
 function mergeWatchedFolders (filenames) {
